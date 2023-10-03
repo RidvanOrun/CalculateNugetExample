c73cf9daea7697203adccf34a6a4bd187a5a4405
Rıdvan Örün <ridvan.orun@outlook.com>
2023-10-03 11:46:16 +0300

    Add project files.

---
+++ CalculateNugetExample/Math.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateNugetExample
+{
+    public class Math
+    {
+
+        public static int Addition(int val1, int val2) { return val1 + val2; }
+        public static int Subtraction(int val1, int val2) { return val1 - val2; }
+    }
+}
